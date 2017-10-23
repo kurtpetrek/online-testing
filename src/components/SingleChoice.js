b5dd276d4c0636d8c4e84ed2260b1db1beef8c5a
Kurt Petrek <kurtpetrek@gmail.com>
2017-10-23 18:26:48 -0400

    Creates basic SingleChoice option

---
+++ src/components/SingleChoice.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+import StyledCheckBox from "./StyledCheckBox";
+
+class SingleChoice extends Component {
+  constructor(props) {
+    super(props);
+    var data = props.questionsData;
+    this.state = {
+      answer: ""
+    };
+    this.question = data.question;
+    this.options = data.options;
+    this.index = parseInt(props.indexKey, 10);
+    this.onAnswer = props.onAnswer;
+  }
+
+  onOptionClick = e => {
+    const answer = e.target.value;
+    this.setState(
+      prevState => {
+        prevState.answer = answer;
+        return prevState;
+      },
+      () => {
+        this.onAnswer(this.state.answer, this.index);
+      }
+    );
+  };
+
+  render() {
+    const options = this.options.map((val, i, a) => {
+      let checked = false;
+      if (this.state.answer === val) {
+        checked = true;
+      }
+      return (
+        <StyledCheckBox
+          handleInput={this.onOptionClick}
+          checked={checked}
+          key={val}
+          value={val}
+        >
+          {val}
+        </StyledCheckBox>
+      );
+    });
+
+    return (
+      <div>
+        <h3>{this.question}</h3>
+        {options}
+      </div>
+    );
+  }
+}
+
+export default SingleChoice;
--- src/components/TestView.js
+++ src/components/TestView.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
 
-import questions from './../data/questions';
+import questions from "./../data/questions";
 
-import Button from './Button';
-
+import Button from "./Button";
+import SingleChoice from "./SingleChoice";
 
 class TestView extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      answers: []
+    };
   }
+
+  handleAnswer = (value, index) => {
+    this.setState(prevState => {
+      prevState.answers[index] = value;
+      return prevState;
+    });
+  };
 
   render() {
     return (
       <div>
-        <h1>HEllo</h1>
-        <p>{questions[0].question}</p>
+        <h1>Hello</h1>
+        <SingleChoice
+          questionsData={questions[0]}
+          indexKey="0"
+          onAnswer={this.handleAnswer}
+        />
       </div>
     );
   }
