eb6b46fbfe647615ca8bb7759fcf41fd24b55919
Kurt Petrek <kurtpetrek@gmail.com>
2017-10-23 21:06:08 -0400

    adds basic styles to landing

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,31 +11,14 @@ const StyledButton = styled.button`
   cursor: pointer;
   transition: 0.25s;
   will-change: transform;
-  ${props => props.italic && "font-style: italic;"}
-  box-shadow: -1px 1px 0px #333,
-  -2px 2px 0px #333,
-  -3px 3px 0px #333,
-  -4px 4px 0px #333;
-  &:hover {
+  ${props => props.italic && "font-style: italic;"} &:hover {
     transform: scale(1.05);
-    box-shadow: -1px 1px 0px #333,
-    -2px 2px 0px #333,
-    -3px 3px 0px #333,
-    -4px 4px 0px #333,
-    -5px 5px 0px #333,
-    -6px 6px 0px #333;
   }
   &:active {
     transform: scale(0.95);
-    box-shadow: -1px 1px 0px #222,
-    -2px 2px 0px #222;
   }
 `;
 
 export default function Button(props) {
-  return (
-    <StyledButton {...props}>
-      {props.children}
-    </StyledButton>
-  );
+  return <StyledButton {...props}>{props.children}</StyledButton>;
 }
--- src/components/LandingView.js
+++ src/components/LandingView.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
 
-import Button from './Button';
+import Button from "./Button";
 
 const LandingContainer = styled.div`
   min-height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
+
+  main {
+    width: 100%;
+    padding: 1rem;
+    max-width: 700px;
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  p {
+    font-size: 18px;
+  }
+
+  .button-container {
+    text-align: center;
+    padding: 1rem;
+  }
 `;
 
 class LandingView extends Component {
@@ -19,7 +38,20 @@ class LandingView extends Component {
   render() {
     return (
       <LandingContainer>
-        <Button onClick={this.handleTestStart}>Start Test</Button>
+        <main>
+          <h1>
+            Project Code <br />
+            Knowledge Assessment
+          </h1>
+          <p>
+            This test is aimed to measure the knowledge gained through the
+            Project Code Program and does not effect your standing in the
+            program.
+          </p>
+          <div className="button-container">
+            <Button onClick={this.handleTestStart}>Start Test</Button>
+          </div>
+        </main>
       </LandingContainer>
     );
   }
